4d2854a532415791ef4b09b69a3e202d280e0296
David Martínez Gilo <davidmgilo@gmail.com>
2016-12-28 14:22:51 +0100

    ok

--- HelloWorldVS15/HelloWorldVS15/HelloWorldVS15.cpp
+++ HelloWorldVS15/HelloWorldVS15/HelloWorldVS15.cpp
@@ -1,11 +1,19 @@
 // HelloWorldVS15.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include "stdafx.h" //Precompiled header @see http://stackoverflow.com/questions/4726155/whats-the-use-for-stdafx-h-in-visual-studio
 
+using namespace std;
 
 int main()
 {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	printf("Hello world!! \n");
+	int valor;
+	scanf("%d", &valor);
+	printf("%d \n", valor);
+	cin >> valor;
+	cout << valor << endl;
     return 0;
 }
 
