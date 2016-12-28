39eebea585a6bcda4d18bf19255fb42c87721770
David Martínez Gilo <davidmgilo@gmail.com>
2016-12-28 20:04:37 +0100

    sync

--- HelloWorldVS15/HelloWorldVS15/HelloWorldVS15.cpp
+++ HelloWorldVS15/HelloWorldVS15/HelloWorldVS15.cpp
@@ -3,17 +3,15 @@
 
 #include "stdafx.h" //Precompiled header @see http://stackoverflow.com/questions/4726155/whats-the-use-for-stdafx-h-in-visual-studio
 
-using namespace std;
-
 int main()
 {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	printf("Hello world!! \n");
 	int valor;
 	scanf("%d", &valor);
 	printf("%d \n", valor);
-	cin >> valor;
-	cout << valor << endl;
+	std::cin >> valor;
+	std::cout << valor << std::endl;
     return 0;
 }
 
